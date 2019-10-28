024b27df5cd804a724eff7126f310f56ee4fb503
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-10-28 21:39:31 +0700

    UI DEVELOPER - 5.XML Sign In

--- app/src/main/java/com/takatutustudio/lat01tiketsaya/GetStartedAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/GetStartedAct.java
@@ -2,13 +2,26 @@ package com.takatutustudio.lat01tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStartedAct extends AppCompatActivity {
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        btnSignIn   = findViewById(R.id.btn_sign_in);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotosign = new Intent(GetStartedAct.this, SigInAct.class);
+                startActivity(gotosign);
+            }
+        });
     }
 }
