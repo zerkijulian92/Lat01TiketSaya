cb8905ca53580efb69c8cef04f9afa7aadd54c3d
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-10-30 14:14:50 +0700

    UI DEVELOPER II - 3. XML Success Register

--- app/src/main/java/com/takatutustudio/lat01tiketsaya/RegisterTwoAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/RegisterTwoAct.java
@@ -2,12 +2,15 @@ package com.takatutustudio.lat01tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RegisterTwoAct extends AppCompatActivity {
     LinearLayout btnBack;
+    Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,7 @@ public class RegisterTwoAct extends AppCompatActivity {
         setContentView(R.layout.activity_register_two);
 
         btnBack     = findViewById(R.id.btn_back);
+        btnContinue = findViewById(R.id.btn_continue);
 
         //Fungsi btnBack
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +27,15 @@ public class RegisterTwoAct extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        //Goto SuccesRegister
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotosuccesregister = new Intent(RegisterTwoAct.this, SuccesRegisterAct.class);
+                startActivity(gotosuccesregister);
+            }
+        });
+
     }
 }
