5032fae705740b7710210279ecba1df7342f2947
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-10-31 22:43:55 +0700

    UI DEVELOPER III - 2. XML Ticket Detail

--- app/src/main/java/com/takatutustudio/lat01tiketsaya/HomeAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/HomeAct.java
@@ -2,13 +2,28 @@ package com.takatutustudio.lat01tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class HomeAct extends AppCompatActivity {
+    LinearLayout btnTicketPisa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnTicketPisa   = findViewById(R.id.btn_ticket_pisa);
+
+        //goto Ticket Detail
+        btnTicketPisa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotopisaticket = new Intent(HomeAct.this, TicketDetailAct.class);
+                startActivity(gotopisaticket);
+            }
+        });
     }
 }
