70055d8422093d5b970eda375b569495d5260c69
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-10-31 17:41:34 +0700

    UI Developer III - 1. Home Screen

--- app/src/main/java/com/takatutustudio/lat01tiketsaya/SigInAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/SigInAct.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SigInAct extends AppCompatActivity {
     TextView btnCreateNewAccount;
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +18,23 @@ public class SigInAct extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         btnCreateNewAccount = findViewById(R.id.btn_create_new_account);
+        btnSignIn = findViewById(R.id.btn_sign_in);
+
+        //Goto RegisterOne
         btnCreateNewAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent gotoregisterone = new Intent(SigInAct.this, RegisterOneAct.class);
                 startActivity(gotoregisterone);
+            }
+        });
+
+        //Goto HomeAct
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotohome = new Intent(SigInAct.this, HomeAct.class);
+                startActivity(gotohome);
             }
         });
     }
--- app/src/main/java/com/takatutustudio/lat01tiketsaya/SuccesRegisterAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/SuccesRegisterAct.java
@@ -2,13 +2,27 @@ package com.takatutustudio.lat01tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SuccesRegisterAct extends AppCompatActivity {
+    Button btnExplore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_succes_register);
+
+        btnExplore      = findViewById(R.id.btn_explore);
+
+        btnExplore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotohome = new Intent(SuccesRegisterAct.this, HomeAct.class);
+                startActivity(gotohome);
+            }
+        });
     }
 }
