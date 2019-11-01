1865073c9bc390f45578242deb170052f9870d8e
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-11-01 23:13:29 +0700

    UI DEVELOPER III - 3. XML Checkout

--- app/src/main/java/com/takatutustudio/lat01tiketsaya/TicketDetailAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/TicketDetailAct.java
@@ -2,13 +2,28 @@ package com.takatutustudio.lat01tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TicketDetailAct extends AppCompatActivity {
+    Button btnBuyTicket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_detail);
+
+        btnBuyTicket    = findViewById(R.id.btn_buy_ticket);
+
+        //Goto Checkout
+        btnBuyTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotocheckout = new Intent(TicketDetailAct.this, TicketCheckoutAct.class);
+                startActivity(gotocheckout);
+            }
+        });
     }
 }
