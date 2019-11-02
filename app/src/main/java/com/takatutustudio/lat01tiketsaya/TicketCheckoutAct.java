e9e08dff268439bc7d741d0c6d71f19963507673
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-11-02 12:19:42 +0700

    UI DEVELOPER III - 4. XML Succes Ticket

--- app/src/main/java/com/takatutustudio/lat01tiketsaya/TicketCheckoutAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/TicketCheckoutAct.java
@@ -2,13 +2,28 @@ package com.takatutustudio.lat01tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TicketCheckoutAct extends AppCompatActivity {
+    Button btnBuyTicket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_checkout);
+
+        btnBuyTicket    = findViewById(R.id.btn_buy_ticket);
+
+        //Goto SuccesBuyTicket
+        btnBuyTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotosuccesbuyticket = new Intent(TicketCheckoutAct.this, SuccesBuyTicketAct.class);
+                startActivity(gotosuccesbuyticket);
+            }
+        });
     }
 }
