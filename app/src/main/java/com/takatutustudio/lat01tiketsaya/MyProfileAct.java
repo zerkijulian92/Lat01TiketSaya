9e60639d904f48213ff828f9c57abdec963ba2b1
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-11-05 16:42:26 +0700

    UI DEVELOPER IV - 3.XML Edit Profile

--- app/src/main/java/com/takatutustudio/lat01tiketsaya/MyProfileAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/MyProfileAct.java
@@ -6,10 +6,12 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MyProfileAct extends AppCompatActivity {
     LinearLayout ItemMyTicket;
+    Button btnEditProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +19,23 @@ public class MyProfileAct extends AppCompatActivity {
         setContentView(R.layout.activity_my_profile);
 
         ItemMyTicket    = findViewById(R.id.item_my_ticket);
+        btnEditProfile  = findViewById(R.id.btn_edit_profile);
 
-        //Goto My Profile
+        //Goto My Ticket Detail
         ItemMyTicket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent gotomyticket = new Intent(MyProfileAct.this, MyTicketDetailAct.class);
                 startActivity(gotomyticket);
+            }
+        });
+
+        //Goto Edit Profile
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoeditprofile = new Intent(MyProfileAct.this, EditProfileAct.class);
+                startActivity(gotoeditprofile);
             }
         });
     }
