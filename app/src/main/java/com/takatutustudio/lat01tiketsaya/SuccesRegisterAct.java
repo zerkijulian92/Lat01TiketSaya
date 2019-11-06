d93d9cb181ad75fe7047cf7e71c277b6ad8a2b36
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-11-06 14:58:37 +0700

    UI ANIMATION V - 3. Succes SignUp

--- app/src/main/java/com/takatutustudio/lat01tiketsaya/SuccesRegisterAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/SuccesRegisterAct.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SuccesRegisterAct extends AppCompatActivity {
+    Animation appSplash, bottomToTop, topToBottom;
+    ImageView iconSucces;
+    TextView appTitle, appSubtitle;
     Button btnExplore;
 
     @Override
@@ -15,8 +22,24 @@ public class SuccesRegisterAct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_succes_register);
 
+        //Load Element
+        iconSucces      = findViewById(R.id.icon_succes);
+        appTitle        = findViewById(R.id.app_title);
+        appSubtitle     = findViewById(R.id.app_subtitle);
         btnExplore      = findViewById(R.id.btn_explore);
 
+        //Load Element Animasi
+        appSplash       = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        bottomToTop     = AnimationUtils.loadAnimation(this, R.anim.bottom_to_top);
+        topToBottom     = AnimationUtils.loadAnimation(this, R.anim.top_to_bottom);
+
+        //Run Animasi
+        iconSucces.startAnimation(appSplash);
+        appTitle.startAnimation(topToBottom);
+        appSubtitle.startAnimation(topToBottom);
+        btnExplore.startAnimation(bottomToTop);
+
+        //Goto Home
         btnExplore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
