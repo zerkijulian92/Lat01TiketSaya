3bceb61e6da940a65d33ae14a6a8673b5b00ab2b
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-11-06 23:25:03 +0700

    UI ANIMATION v - 5. Succes Buy

--- app/src/main/java/com/takatutustudio/lat01tiketsaya/SuccesBuyTicketAct.java
+++ app/src/main/java/com/takatutustudio/lat01tiketsaya/SuccesBuyTicketAct.java
@@ -3,12 +3,40 @@ package com.takatutustudio.lat01tiketsaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SuccesBuyTicketAct extends AppCompatActivity {
+    Animation appSplash, bottomToTop, topToBottom;
+    ImageView iconSucces;
+    TextView appTitle, appSubtitle;
+    Button btnEditProfile, btnMyDashboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_succes_buy_ticket);
+
+        //Registrasi element
+        iconSucces      = findViewById(R.id.icon_succes);
+        appTitle        = findViewById(R.id.app_title);
+        appSubtitle     = findViewById(R.id.app_subtitle);
+        btnEditProfile  = findViewById(R.id.btn_edit_profile);
+        btnMyDashboard  = findViewById(R.id.btn_my_dashboard);
+
+        //Regitrasi Animasi
+        appSplash       = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        bottomToTop     = AnimationUtils.loadAnimation(this, R.anim.bottom_to_top);
+        topToBottom     = AnimationUtils.loadAnimation(this, R.anim.top_to_bottom);
+
+        //Run Animasi
+        iconSucces.startAnimation(appSplash);
+        appTitle.startAnimation(topToBottom);
+        appSubtitle.startAnimation(topToBottom);
+        btnEditProfile.startAnimation(bottomToTop);
+        btnMyDashboard.startAnimation(bottomToTop);
     }
 }
